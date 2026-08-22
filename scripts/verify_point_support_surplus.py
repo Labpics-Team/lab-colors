7ad9efd611fd33564a4309a67db72947c2934095
Daniel <daniel@labpics.team>
2026-08-22 09:35:34 +0300

    ci: force GitHub mergeability recomputation

--- scripts/verify_point_support_surplus.py
+++ scripts/verify_point_support_surplus.py
@@ -58,7 +58,7 @@ SITE_ID = "point-support-retained-reference-surplus-v1"
 SOURCE_BINDING_LAW = "point-support-rust-whole-file-semantic-cone-v2"
 SOURCE_BINDING_DOMAIN = b"labcolors.point-support.rust-whole-file-semantic-cone.v2"
 EXPECTED_SOURCE_CAPSULE_SHA256 = (
-    "e39edcd3e0136d8ef0af59990caaf2ede871674d9469c658eeab1e0dac7ab037"
+    "70950c681c59ffdb5597f9c665bfd996c1db280da0d8f621cff895b05f7c46e3"
 )
 EXPECTED_Q55_PROOF_SHA256 = (
     "e0f8a01ae106b9147606ac15094968fef1ce6575a568046fa0247d1eb8845e8b"
